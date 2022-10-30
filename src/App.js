959fd7d0fe35018d2ffd84140d368839229a342c
svvdevell <svvdevel@gmail.com>
2022-10-30 15:17:05 +0200

    added task

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Title from "./components/Title";
+
+function App() {
+  return (<div>
+    <Title name="Products" type="bold"/>
+    <Title name="Products" type="normal"/>
+  </div>);
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+const Title = ({name, type}) => {
+    return (
+        <h1 style ={{fontWeight: type}}>{name}</h1>
+    )
+};
+export default Title
